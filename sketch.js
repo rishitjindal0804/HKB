5b42680c4762ef8aedd0bcf5023aad121c6d40e4
rishitjindal0804 <72559202+rishitjindal0804@users.noreply.github.com>
2020-10-13 11:11:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
    background = createSprite(width/2,height/2,width*5,height);
   background.addImage(back); 
   
-  monkey = createSprite(width-80,height-80,20,50);
+  monkey = createSprite(width-50,height-80,20,50);
   monkey.addAnimation("running", monkey_running);
   monkey.scale = 0.2;
   
